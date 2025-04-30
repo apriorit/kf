d4e3f583f397c8c3df49bd3ff2fbd65ad268bd46
Serhii Malyi <maliy.sergey@apriorit.com>
2025-04-30 17:36:04 +0300

    Add shared and exclusive locks for EResource

---
+++ include/kf/EResourceExclusiveLock.h
@@ -0,0 +1,32 @@
+#pragma once
+
+namespace kf
+{
+    //////////////////////////////////////////////////////////////////////////
+    // EResourceExclusiveLock
+
+    class EResourceExclusiveLock
+    {
+    public:
+        EResourceExclusiveLock(_In_ EResource& resource) : m_resource(resource)
+        {
+            KeEnterCriticalRegion();
+            m_resource.acquireExclusive();
+        }
+
+        EResourceExclusiveLock(EResourceExclusiveLock&& other) = default;
+
+        ~EResourceExclusiveLock()
+        {
+            m_resource.release();
+            KeLeaveCriticalRegion();
+        }
+
+    private:
+        EResourceExclusiveLock(const EResourceExclusiveLock&);
+        EResourceExclusiveLock& operator=(const EResourceExclusiveLock&);
+
+    private:
+        EResource& m_resource;
+    };
+}
