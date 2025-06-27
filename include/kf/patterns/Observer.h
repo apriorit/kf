eb810633e222cac977f9c7b6c3e9b969a363b1c2
Serhii <serezhamymail@gmail.com>
2025-06-27 15:18:35 +0300

    Add Observer pattern (#9)
    * Add Observer pattern
    
    * Add public inheritance of IObservable for MonoObservable
    
    ---------
    
    Co-authored-by: Serhii Malyi <maliy.sergey@apriorit.com>

---
+++ include/kf/patterns/Observer.h
@@ -0,0 +1,52 @@
+#pragma once
+
+namespace kf
+{
+    template<typename... T>
+    class IObserver
+    {
+    public:
+        virtual ~IObserver() = default;
+
+        virtual void onNotify(T... t) = 0;
+    };
+
+    template<typename... T>
+    class IObservable
+    {
+    public:
+        virtual ~IObservable() = default;
+
+        virtual void attach(IObserver<T...>*) = 0;
+        virtual void detach(IObserver<T...>*) = 0;
+    };
+
+    template<typename... T>
+    class MonoObservable : public IObservable<T...>
+    {
+    public:
+        void attach(IObserver<T...>* observer) override
+        {
+            assert(!m_observer);
+            m_observer = observer;
+        }
+
+        void detach(IObserver<T...>* observer) override
+        {
+            assert(!m_observer || m_observer == observer);
+            m_observer = nullptr;
+        }
+
+    protected:
+        void notify(T... t)
+        {
+            if (m_observer)
+            {
+                m_observer->onNotify(t...);
+            }
+        }
+
+    private:
+        IObserver<T...>* m_observer = nullptr;
+    };
+}
