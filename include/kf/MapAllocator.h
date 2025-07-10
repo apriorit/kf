b4580a2f6d793726d27375dc00802b8104237628
Serhii Malyi <maliy.sergey@apriorit.com>
2025-07-10 15:47:09 +0300

    Add wrapper for std::map

---
+++ include/kf/MapAllocator.h
@@ -0,0 +1,89 @@
+#pragma once
+#include <kf/stl/memory>
+
+namespace kf
+{
+    struct AllocatorState
+    {
+        std::unique_ptr<char[]> buffer;
+        size_t size;
+    };
+
+    template<typename T, POOL_TYPE poolType>
+    class MapAllocator
+    {
+    public:
+        using value_type = T;
+
+        MapAllocator() = default;
+
+        // Needed because allocator has extra parameter for POOL_TYPE
+        template <typename U>
+        struct rebind
+        {
+            using other = MapAllocator<U, poolType>;
+        };
+
+        friend class MapAllocator;
+
+        template<typename U>
+        MapAllocator(const MapAllocator<U, poolType>& other)
+            : m_state(other.m_state)
+        {
+        }
+
+        bool init()
+        {
+            m_state = make_shared<AllocatorState, poolType>();
+            return m_state != nullptr;
+        }
+
+        T* allocate(std::size_t n)
+        {
+            if (!m_state || !m_state->buffer)
+            {
+                return nullptr;
+            }
+
+            if (m_state->size < n)
+            {
+                return nullptr;
+            }
+
+            auto buffer = reinterpret_cast<T*>(m_state->buffer.release());
+            m_state->size = 0;
+            return buffer;
+        }
+
+        void deallocate(T*, std::size_t)
+        {
+            m_state->buffer.reset();
+            m_state->size = 0;
+        }
+
+        bool prepareMemory(std::size_t size)
+        {
+            if (!m_state)
+            {
+                return false;
+            }
+
+            if (m_state->buffer)
+            {
+                return true;
+            }
+
+            m_state->buffer.reset(new(poolType) char[size]);
+            if (!m_state->buffer)
+            {
+                return false;
+            }
+            m_state->size = size;
+
+            return true;
+        }
+
+    private:
+        std::shared_ptr<AllocatorState> m_state;
+    };
+}
