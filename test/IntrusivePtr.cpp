7ddf5370fa59cde06063c9c68365bc764cd5ef06
Denys Bielyshev <belyshev.denis@apriorit.com>
2025-08-04 21:58:24 +0300

    implement tests for methods of intrusive_ptr

---
+++ test/IntrusivePtr.cpp
@@ -0,0 +1,130 @@
+#include "pch.h"
+#include <kf/stl/new>
+#include <kf/boost/intrusive_ptr.hpp>
+#include <kf/boost/intrusive_ref_counter.hpp>
+#include <kf/UString.h>
+
+struct IntrusivePtrTestStruct : public boost::intrusive_ref_counter<IntrusivePtrTestStruct>
+{
+    kf::UString<PagedPool> str;
+    UINT64 number = 0;
+};
+
+using IntrusivePtrTestStructPtr = boost::intrusive_ptr<IntrusivePtrTestStruct>;
+
+// TODO: implement benchmark that tests the usage of the same object in several threads
+
+SCENARIO("intrusive_ptr: methods")
+{
+    GIVEN("two IntrusivePtrTestStruct are initialized")
+    {
+        IntrusivePtrTestStructPtr struct1(new(PagedPool) IntrusivePtrTestStruct);
+        REQUIRE_NT_SUCCESS(struct1->str.init(L"42"));
+        struct1->number = 42;
+
+        IntrusivePtrTestStructPtr struct2(new(PagedPool) IntrusivePtrTestStruct);
+        REQUIRE_NT_SUCCESS(struct2->str.init(L"533"));
+        struct2->number = 533;
+
+        WHEN("operator bool is called")
+        {
+            THEN("it returns true for non-empty object")
+            {
+                REQUIRE(struct1);
+                REQUIRE(struct2);
+            }
+        }
+
+        WHEN("get is called")
+        {
+            auto ptr1 = struct1.get();
+            auto ptr2 = struct2.get();
+
+            THEN("pointers are not nullptr")
+            {
+                REQUIRE(ptr1 != nullptr);
+                REQUIRE(ptr2 != nullptr);
+            }
+
+            THEN("pointers are different")
+            {
+                REQUIRE(ptr1 != ptr2);
+            }
+        }
+
+        WHEN("detach is called")
+        {
+            auto ptr = struct1.detach();
+
+            THEN("the object doesn't belong to the smart pointer")
+            {
+                REQUIRE(struct1.get() == nullptr);
+            }
+
+            THEN("detached object is accessible and valid")
+            {
+                REQUIRE(ptr != nullptr);
+                REQUIRE(ptr->str.equals(L"42"));
+                REQUIRE(ptr->number == 42);
+            }
+
+            delete ptr;
+        }
+
+        WHEN("reset is called on the same object")
+        {
+            struct1.reset(struct2.detach());
+
+            THEN("the holder of the donor is nullptr")
+            {
+                REQUIRE(struct2.get() == nullptr);
+            }
+
+            THEN("the holder of recipient holds the donor")
+            {
+                REQUIRE(struct1.get() != nullptr);
+                REQUIRE(struct1->str.equals(L"533"));
+                REQUIRE(struct1->number == 533);
+            }
+        }
+
+        WHEN("swap is called")
+        {
+            struct1.swap(struct2);
+
+            THEN("the content is swapped")
+            {
+                REQUIRE(struct1->str.equals(L"533"));
+                REQUIRE(struct1->number == 533);
+                REQUIRE(struct2->str.equals(L"42"));
+                REQUIRE(struct2->number == 42);
+            }
+        }
+
+        WHEN("operator= is called")
+        {
+            struct1 = struct2.detach();
+
+            THEN("the holder of recipient holds the donor")
+            {
+                REQUIRE(struct1.get() != nullptr);
+                REQUIRE(struct1->str.equals(L"533"));
+                REQUIRE(struct1->number == 533);
+            }
+        }
+
+        WHEN("operator* is called")
+        {
+            auto& ref1 = *struct1;
+            auto& ref2 = *struct2;
+
+            THEN("it dereferences the proper object")
+            {
+                REQUIRE(ref1.str.equals(L"42"));
+                REQUIRE(ref1.number == 42);
+                REQUIRE(ref2.str.equals(L"533"));
+                REQUIRE(ref2.number == 533);
+            }
+        }
+    }
+}
