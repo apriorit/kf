2c5bfa2e0d8e65c70c42ef25958700d25fb5c1bf
Vlada Kanivets <vlada.kanivets@apriorit.com>
2025-08-11 16:43:36 +0200

    add tests for Singleton

---
+++ test/SingletonTest.cpp
@@ -0,0 +1,49 @@
+#include "pch.h"
+#include <kf/Singleton.h>
+#include <kf/stl/new>
+
+class TestSingleton : public kf::Singleton<TestSingleton>
+{
+public:
+    int value = 0;
+};
+
+SCENARIO("Testing Singleton functionality")
+{
+    WHEN("A TestSingleton object was not created yet")
+    {
+        THEN("getInstance returns nullptr")
+        {
+            REQUIRE(TestSingleton::getInstance() == nullptr);
+        }
+    }
+
+    WHEN("A TestSingleton object is created")
+    {
+        TestSingleton singleton;
+
+        THEN("getInstance returns pointer to the created instance")
+        {
+            REQUIRE(TestSingleton::getInstance() == &singleton);
+        }
+
+        THEN("Singleton instance member can be accessed")
+        {
+            singleton.value = 42;
+            REQUIRE(TestSingleton::getInstance()->value == 42);
+        }
+    }
+
+    WHEN("The TestSingleton object is destroyed")
+    {
+        TestSingleton* dynamicSingleton = new(NonPagedPoolNx) TestSingleton();
+        REQUIRE(TestSingleton::getInstance() == dynamicSingleton);
+
+        delete dynamicSingleton;
+
+        THEN("getInstance returns nullptr after destruction")
+        {
+            REQUIRE(TestSingleton::getInstance() == nullptr);
+        }
+    }
+}
