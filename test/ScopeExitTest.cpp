4c7ca668bd3b57db14962ce410c126d4e2ee0876
Vlada Kanivets <vlada.kanivets@apriorit.com>
2025-08-12 15:37:09 +0200

    refactoring

--- test/ScopeExitTest.cpp
+++ test/ScopeExitTest.cpp
@@ -1,76 +1,43 @@
 #include "pch.h"
 #include <kf/ScopeExit.h>
-#include <kf/USimpleString.h>
-
-static int g_functionCallCount = 0;
-
-void testFunction()
-{
-    ++g_functionCallCount;
-}
-
-SCENARIO("kf::ScopeExit")
-{
-    GIVEN("A ScopeExit with a valid function")
-    {
-        g_functionCallCount = 0;
-
-        WHEN("The ScopeExit object is created")
-        {
-            {
-                auto scopeExit = kf::MakeScopeExit() += [] { testFunction(); };
-                THEN("The function is not called immediately")
-                {
-                    REQUIRE(g_functionCallCount == 0);
-                }
-            }
-
-            THEN("The function is called upon destruction")
-            {
-                REQUIRE(g_functionCallCount == 1);
-            }
-        }
-    }
-}
 
 SCENARIO("SCOPE_EXIT macro")
 {
     GIVEN("A block of code with SCOPE_EXIT")
     {
-        g_functionCallCount = 0;
-
         WHEN("The block is executed")
         {
+            int a = 0;
+
             {
-                SCOPE_EXIT { testFunction(); };
+                SCOPE_EXIT{ a++; };
             }
 
             THEN("The cleanup code should execute after the block")
             {
-                REQUIRE(g_functionCallCount == 1);
+                REQUIRE(a == 1);
             }
         }
 
         WHEN("Multiple SCOPE_EXIT macros are used in the same block")
         {
-            kf::USimpleString str;
-            kf::USimpleString expectedStr = L"Second scope exit. First scope exit.";
+            int a = 1;
 
             {
                 SCOPE_EXIT
                 {
-                    str.concat(L"First scope exit.");
+                    a *= 5;
                 };
 
                 SCOPE_EXIT
                 {
-                    str.concat(L"Second scope exit. ");
+                    a += 2;
                 };
             }
 
             THEN("The cleanup code should execute in reverse order of declaration")
             {
-                REQUIRE(str.equals(expectedStr));
+                REQUIRE(a == 15);
             }
         }
     }
