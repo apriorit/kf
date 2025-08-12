e1a773528f690f18db9708d09c27d6a0db2bdde2
Vlada Kanivets <vlada.kanivets@apriorit.com>
2025-08-12 15:46:42 +0200

    refactoring

--- test/ScopeFailureTest.cpp
+++ test/ScopeFailureTest.cpp
@@ -1,96 +1,41 @@
 #include "pch.h"
 #include <kf/ScopeFailure.h>
 
-namespace
+SCENARIO("SCOPE_FAILURE macro")
 {
-    static int g_functionCallCount = 0;
-
-    void testFunction()
-    {
-        ++g_functionCallCount;
-    }
-
-}
-
-SCENARIO("kf::MakeScopeFailure")
-{
-    GIVEN("A MakeScopeFailure with a valid function and unsuccessful NTSTATUS")
-    {
-        g_functionCallCount = 0;
-
-        WHEN("The ScopeFailure object is created")
-        {
-            {
-                NTSTATUS status = STATUS_ACCESS_DENIED;
-                auto scopeExit = kf::MakeScopeFailure(status) += [] { testFunction(); };
-                THEN("The function is not called immediately")
-                {
-                    REQUIRE(g_functionCallCount == 0);
-                }
-            }
-
-            THEN("The function is called upon destruction")
-            {
-                REQUIRE(g_functionCallCount == 1);
-            }
-        }
-    }
-
-    GIVEN("A MakeScopeFailure with a valid function and successful NTSTATUS")
-    {
-        g_functionCallCount = 0;
-
-        WHEN("The ScopeFailure object is created")
-        {
-            {
-                NTSTATUS status = STATUS_SUCCESS;
-                auto scopeExit = kf::MakeScopeFailure(status) += [] { testFunction(); };
-                THEN("The function is not called immediately")
-                {
-                    REQUIRE(g_functionCallCount == 0);
-                }
-            }
-
-            THEN("The function is not called upon destruction")
-            {
-                REQUIRE(g_functionCallCount == 0);
-            }
-        }
-    }
-
     GIVEN("SCOPE_FAILURE macro with an unsuccessful NTSTATUS")
     {
         NTSTATUS status = STATUS_ACCESS_DENIED;
-        g_functionCallCount = 0;
+        int value = 0;
 
         {
             SCOPE_FAILURE(status)
             {
-                testFunction();
+                value++;
             };
         }
 
         THEN("Scoped function should be called")
         {
-            REQUIRE(g_functionCallCount == 1);
+            REQUIRE(value == 1);
         }
     }
 
     GIVEN("SCOPE_FAILURE macro with a successful NTSTATUS")
     {
         NTSTATUS status = STATUS_SUCCESS;
-        g_functionCallCount = 0;
+        int value = 0;
 
         {
             SCOPE_FAILURE(status)
             {
-                testFunction();
+                value++;
             };
         }
 
         THEN("Scoped function should not be called")
         {
-            REQUIRE(g_functionCallCount == 0);
+            REQUIRE(value == 0);
         }
     }
 
@@ -98,28 +43,28 @@ SCENARIO("kf::MakeScopeFailure")
     {
         NTSTATUS successful = STATUS_SUCCESS;
         NTSTATUS failed = STATUS_ACCESS_DENIED;
-        g_functionCallCount = 0;
+        int value = 0;
 
         {
             SCOPE_FAILURE(failed)
             {
-                testFunction();
+                value++;
             };
 
             SCOPE_FAILURE(successful)
             {
-                testFunction();
+                value++;
             };
 
             SCOPE_FAILURE(failed)
             {
-                testFunction();
+                value++;
             };
         }
 
         THEN("Scoped function should be called twice")
         {
-            REQUIRE(g_functionCallCount == 2);
+            REQUIRE(value == 2);
         }
     }
 }
