dd8cfc9fa28a953c68e1ef7c90d5a8e5e2c90322
Vlada Kanivets <vlada.kanivets@apriorit.com>
2025-08-13 17:06:28 +0200

    add more tests

--- test/TextDetectorTest.cpp
+++ test/TextDetectorTest.cpp
@@ -142,4 +142,80 @@ SCENARIO("TextDetector::isText")
             REQUIRE(!kf::TextDetector::isText(std::as_bytes(std::span{ kData })));
         }
     }
+
+    GIVEN("Valid UTF-16BE text without BOM")
+    {
+        constexpr uint8_t kData[] = {
+            0x00, 'T',
+            0x00, 'e',
+            0x00, 's',
+            0x00, 't'
+        };
+
+        THEN("Text is detected")
+        {
+            REQUIRE(kf::TextDetector::isText(std::as_bytes(std::span{ kData })));
+        }
+    };
+
+    GIVEN("UTF-16BE text without BOM and invalid char")
+    {
+        constexpr uint8_t kData[] = {
+            0x00, 0x01, // invalid char
+            0x00, 'a'
+        };
+
+        THEN("Not a text is detected")
+        {
+            REQUIRE(!kf::TextDetector::isText(std::as_bytes(std::span{ kData })));
+        }
+    };
+
+    GIVEN("Valid UTF-16LE text without BOM")
+    {
+        constexpr uint8_t kData[] = {
+            'T', 0x00,
+            'e', 0x00,
+            's', 0x00,
+            't', 0x00
+        };
+
+        THEN("Text is detected")
+        {
+            REQUIRE(kf::TextDetector::isText(std::as_bytes(std::span{ kData })));
+        }
+    }
+
+    GIVEN("UTF-16LE text without BOM and invalid char")
+    {
+        constexpr uint8_t kData[] = {
+            0x01, 0x00, // invalid char
+            'a', 0x00
+        };
+
+        THEN("Not a text is detected")
+        {
+            REQUIRE(!kf::TextDetector::isText(std::as_bytes(std::span{ kData })));
+        }
+    };
+
+    GIVEN("Valid UTF-8 text without BOM")
+    {
+        constexpr uint8_t kData[] = { 'T', 'e', 's', 't' };
+
+        THEN("Text is detected")
+        {
+            REQUIRE(kf::TextDetector::isText(std::as_bytes(std::span{ kData })));
+        }
+    };
+
+    GIVEN("UTF-8 text without BOM and invalid char")
+    {
+        constexpr uint8_t kData[] = { 0x01, 'a', 'b' };
+
+        THEN("Not a text is detected")
+        {
+            REQUIRE(!kf::TextDetector::isText(std::as_bytes(std::span{ kData })));
+        }
+    };
 }
