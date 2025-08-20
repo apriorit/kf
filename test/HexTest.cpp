a23aecce57f7cf364a19917bb1b423bf4bccce53
Sergey Podobry <sergius@apriorit.com>
2025-08-20 14:37:07 +0300

    Add more constants

--- test/HexTest.cpp
+++ test/HexTest.cpp
@@ -2,22 +2,24 @@
 #include <kf/Hex.h>
 #include <kf/UString.h>
 #include <span>
+#include <array>
 
 SCENARIO("Hex::encode")
 {
     GIVEN("A byte array of data")
     {
-        constexpr UCHAR data[] = { 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa,
-                                   0xb, 0xc, 0xd, 0xe, 0xf, 0x42, 0xfe, 0xd0, 0x5e, 0xf1, 0x40 };
-        kf::UString<NonPagedPoolNx> hex;
-        const auto dataSpan = std::as_bytes(std::span{ data });
+        constexpr UCHAR kData[] = { 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa,
+                                    0xb, 0xc, 0xd, 0xe, 0xf, 0x42, 0xfe, 0xd0, 0x5e, 0xf1, 0x40 };
+        const auto dataSpan = std::as_bytes(std::span{ kData });
+        kf::UString<NonPagedPoolNx> hexString;
 
         WHEN("encodeLen of buffer")
         {
             THEN("kf::Hex::encodeLen returns correct length")
             {
+                constexpr size_t kExpectedEncodeLen = sizeof(kData) * 2;
                 const auto encodeLen = kf::Hex::encodeLen(dataSpan);
-                REQUIRE(encodeLen == (sizeof(data) * 2));
+                REQUIRE(encodeLen == kExpectedEncodeLen);
             }
         }
 
@@ -25,20 +27,20 @@ SCENARIO("Hex::encode")
         {
             THEN("kf::Hex::encode return false")
             {
-                REQUIRE(!kf::Hex::encode(dataSpan, hex));
+                REQUIRE(!kf::Hex::encode(dataSpan, hexString));
             }
         }
 
         WHEN("Encoding into UString were buffer size is equal to encodeLen")
         {
             const auto encodeLen = kf::Hex::encodeLen(dataSpan);
-            REQUIRE(encodeLen == (sizeof(data) * 2));
-            REQUIRE_NT_SUCCESS(hex.realloc(encodeLen * sizeof(WCHAR)));
+            REQUIRE_NT_SUCCESS(hexString.realloc(encodeLen * sizeof(WCHAR)));
 
             THEN("kf::Hex::encode return true and correct UString")
             {
-                REQUIRE(kf::Hex::encode(dataSpan, hex));
-                REQUIRE(memcmp(hex.buffer(), L"000102030405060708090A0B0C0D0E0F42FED05EF140", hex.byteLength()) == 0);
+                constexpr WCHAR kExpectedHexString[] = L"000102030405060708090A0B0C0D0E0F42FED05EF140";
+                REQUIRE(kf::Hex::encode(dataSpan, hexString));
+                REQUIRE(memcmp(hexString.buffer(), kExpectedHexString, hexString.byteLength()) == 0);
             }
         }
     }
@@ -48,42 +50,51 @@ SCENARIO("Hex::decode")
 {
     GIVEN("An encoded ASimpleString")
     {
-        const kf::ASimpleString hex("000102030405060708090A0B0C0D0E0F42FED05EF140");
-        constexpr int expectedHexLen = 22;
-        constexpr UCHAR expectedData[expectedHexLen] = { 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa,
-                        0xb, 0xc, 0xd, 0xe, 0xf, 0x42, 0xfe, 0xd0, 0x5e, 0xf1, 0x40 };
+        const kf::ASimpleString hexString("000102030405060708090A0B0C0D0E0F42FED05EF140");
+        constexpr UCHAR kExpectedData[] = { 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa,
+                                            0xb, 0xc, 0xd, 0xe, 0xf, 0x42, 0xfe, 0xd0, 0x5e, 0xf1, 0x40 };
+        constexpr int kExpectedDataLen = ARRAYSIZE(kExpectedData);
 
         WHEN("encodeLen of buffer")
         {
             THEN("kf::Hex::decodeLen returns correct length")
             {
-                const auto decodeLen = kf::Hex::decodeLen(hex);
-                REQUIRE(decodeLen == expectedHexLen);
+                const auto decodeLen = kf::Hex::decodeLen(hexString);
+                REQUIRE(decodeLen == kExpectedDataLen);
+            }
+        }
+
+        WHEN("Decoding into an empty buffer")
+        {
+            std::array<std::byte, 0> buffer;
+            std::span<std::byte> bufferSpan = buffer;
+
+            THEN("kf::Hex::decode returns false")
+            {
+                REQUIRE(!kf::Hex::decode(hexString, bufferSpan));
             }
         }
 
         WHEN("Decoding into a buffer with insufficient size")
         {
-            std::array<std::byte, 0> emptyBuffer;
-            std::span<std::byte> initialSpan(emptyBuffer.data(), emptyBuffer.size());
+            std::array<std::byte, kExpectedDataLen - 1> buffer;
+            std::span<std::byte> bufferSpan = buffer;
 
             THEN("kf::Hex::decode returns false")
             {
-                REQUIRE(!kf::Hex::decode(hex, initialSpan));
+                REQUIRE(!kf::Hex::decode(hexString, bufferSpan));
             }
         }
 
-        WHEN("Decoding into a buffer were size is equal to decodeLen")
+        WHEN("Decoding into a buffer with size equal to decodeLen")
         {
-            const auto decodeLen = kf::Hex::decodeLen(hex);
-            REQUIRE(decodeLen == expectedHexLen);
-            std::array<std::byte, expectedHexLen> buffer;
+            std::array<std::byte, kExpectedDataLen> buffer;
+            std::span<std::byte> bufferSpan = buffer;
 
-            std::span<std::byte> resizedSpan(buffer.data(), buffer.size());
             THEN("kf::Hex::decode returns true and correct data")
             {
-                REQUIRE(kf::Hex::decode(hex, resizedSpan));
-                REQUIRE(memcmp(expectedData, resizedSpan.data(), resizedSpan.size()) == 0);
+                REQUIRE(kf::Hex::decode(hexString, bufferSpan));
+                REQUIRE(memcmp(kExpectedData, bufferSpan.data(), bufferSpan.size()) == 0);
             }
         }
     }
@@ -95,8 +106,9 @@ SCENARIO("Hex::decode")
         THEN("kf::Hex::decode returns false")
         {
             std::array<std::byte, 4> buffer;
-            std::span<std::byte> span(buffer.data(), buffer.size());
-            REQUIRE(!kf::Hex::decode(invalidHex, span));
+            std::span<std::byte> bufferSpan = buffer;
+
+            REQUIRE(!kf::Hex::decode(invalidHex, bufferSpan));
         }
     }
 }
