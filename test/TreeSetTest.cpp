e1c45324c8834d7271c86d9492033396fb940fcb
Sergey Podobry <sergius@apriorit.com>
2026-01-07 20:56:46 +0200

    Update tests

--- test/TreeSetTest.cpp
+++ test/TreeSetTest.cpp
@@ -2,18 +2,18 @@
 #include <kf/TreeSet.h>
 
 using namespace kf;
-using Set = kf::TreeSet<int, PagedPool>;
 
 namespace
 {
-    constexpr int kMinElem = 1;
-    constexpr int kMaxElem = 5;
-    constexpr int kSetSize = kMaxElem - kMinElem + 1;
+    using IntTreeSet = kf::TreeSet<int, PagedPool>;
+
+    constexpr std::array kElements = { 1, 5, 3, 4, 2 };
+    constexpr std::array kNonExistingElements = { -1, 0, 6, 7, 8 };
 }
 
 SCENARIO("TreeSet: all methods")
 {
-    Set set;
+    IntTreeSet set;
 
     GIVEN("empty set")
     {
@@ -23,7 +23,7 @@ SCENARIO("TreeSet: all methods")
 
             THEN("the value is true")
             {
-                REQUIRE(isEmpty == true);
+                REQUIRE(isEmpty);
             }
         }
 
@@ -39,17 +39,17 @@ SCENARIO("TreeSet: all methods")
 
         WHEN("contains is called")
         {
-            auto contains = set.contains(kMinElem);
+            auto contains = set.contains(kElements.front());
 
             THEN("the value is false")
             {
-                REQUIRE(contains == false);
+                REQUIRE(!contains);
             }
         }
 
         WHEN("find is called")
         {
-            auto found = set.find(kMinElem);
+            auto found = set.find(kElements.front());
 
             THEN("the value is nullptr")
             {
@@ -59,11 +59,11 @@ SCENARIO("TreeSet: all methods")
 
         WHEN("remove is called")
         {
-            auto removed = set.remove(kMinElem);
+            auto removed = set.remove(kElements.front());
 
             THEN("the removal should fail")
             {
-                REQUIRE(removed == false);
+                REQUIRE(!removed);
             }
         }
 
@@ -73,7 +73,7 @@ SCENARIO("TreeSet: all methods")
 
             THEN("set is still empty")
             {
-                REQUIRE(set.isEmpty() == true);
+                REQUIRE(set.isEmpty());
                 REQUIRE(set.size() == 0);
             }
         }
@@ -81,106 +81,107 @@ SCENARIO("TreeSet: all methods")
 
     GIVEN("set with elements")
     {
-        for (int elem = kMinElem; elem <= kMaxElem; ++elem)
+        for (int elem : kElements)
         {
             REQUIRE_NT_SUCCESS(set.add(elem));
         }
 
         WHEN("isEmpty is called")
         {
-            auto isEmpty = set.isEmpty();
-
             THEN("the value is false")
             {
-                REQUIRE(isEmpty == false);
+                REQUIRE(!set.isEmpty());
             }
         }
 
         WHEN("size is called")
         {
-            auto size = set.size();
-
             THEN("the size is valid")
             {
-                REQUIRE(size == kSetSize);
+                REQUIRE(set.size() == kElements.size());
             }
         }
 
-        WHEN("contains is called for existing element")
+        WHEN("contains is called for existing elements")
         {
-            auto contains = set.contains(kMinElem + 1);
-
-            THEN("the value should be true")
+            THEN("the values should be true")
             {
-                REQUIRE(contains == true);
+                for (int elem : kElements)
+                {
+                    REQUIRE(set.contains(elem));
+                }
             }
         }
 
-        WHEN("contains is called for a non-existing element")
+        WHEN("contains is called for non-existing elements")
         {
-            auto contains = set.contains(kMaxElem + 1);
-
-            THEN("the value should be false")
+            THEN("the values should be false")
             {
-                REQUIRE(contains == false);
+                for (int elem : kNonExistingElements)
+                {
+                    REQUIRE(!set.contains(elem));
+                }
             }
         }
 
         WHEN("find is called for existing element")
         {
-            auto found = set.find(kMinElem + 1);
-
             THEN("the value should be valid")
             {
-                REQUIRE(found != nullptr);
-                REQUIRE(*found == kMinElem + 1);
+                for (int elem : kElements)
+                {
+                    auto found = set.find(elem);
+                    REQUIRE(found != nullptr);
+                    REQUIRE(*found == elem);
+                }
             }
         }
 
-        WHEN("find is called for a non-existing element")
+        WHEN("find is called for non-existing element")
         {
-            auto found = set.find(kMaxElem + 1);
-
             THEN("the value should be nullptr")
             {
-                REQUIRE(found == nullptr);
+                for (int elem : kNonExistingElements)
+                {
+                    auto found = set.find(elem);
+                    REQUIRE(found == nullptr);
+                }
             }
         }
 
         WHEN("remove is called for existing element")
         {
-            auto removed = set.remove(kMinElem);
+            auto removed = set.remove(kElements.front());
 
             THEN("the removal should succeed")
             {
-                REQUIRE(removed == true);
+                REQUIRE(removed);
             }
 
             THEN("the element is no longer in the set")
             {
-                auto contains = set.contains(kMinElem);
-                REQUIRE(contains == false);
+                REQUIRE(!set.contains(kElements.front()));
             }
 
             THEN("the size is reduced")
             {
-                REQUIRE(set.size() == kSetSize - 1);
+                REQUIRE(set.size() == kElements.size() - 1);
             }
         }
 
         WHEN("remove is called for a non-existing element")
         {
-            auto removed = set.remove(kMaxElem + 1);
+            auto removed = set.remove(kNonExistingElements.front());
 
             THEN("the removal should fail")
             {
-                REQUIRE(removed == false);
+                REQUIRE(!removed);
             }
         }
 
         WHEN("add is called for new element")
         {
-            int newElem = kMaxElem + 1;
+            int newElem = kNonExistingElements.front();
             auto status = set.add(newElem);
 
             THEN("the add should succeed")
@@ -190,19 +191,18 @@ SCENARIO("TreeSet: all methods")
 
             THEN("the element should be accessible")
             {
-                auto contains = set.contains(newElem);
-                REQUIRE(contains == true);
+                REQUIRE(set.contains(newElem));
             }
 
             THEN("the size should increase")
             {
-                REQUIRE(set.size() == kSetSize + 1);
+                REQUIRE(set.size() == kElements.size() + 1);
             }
         }
 
         WHEN("add is called for existing element")
         {
-            auto status = set.add(kMinElem);
+            auto status = set.add(kElements.front());
 
             THEN("the add should succeed (idempotent)")
             {
@@ -211,8 +211,7 @@ SCENARIO("TreeSet: all methods")
 
             THEN("the element should still be accessible")
             {
-                auto contains = set.contains(kMinElem);
-                REQUIRE(contains == true);
+                REQUIRE(set.contains(kElements.front()));
             }
         }
 
@@ -222,91 +221,249 @@ SCENARIO("TreeSet: all methods")
 
             THEN("set is empty")
             {
-                REQUIRE(set.isEmpty() == true);
+                REQUIRE(set.isEmpty());
                 REQUIRE(set.size() == 0);
             }
 
             THEN("valid element is no longer accessible")
             {
-                REQUIRE(set.contains(kMinElem) == false);
+                for (int elem : kElements)
+                {
+                    REQUIRE(!set.contains(elem));
+                }
             }
         }
 
-        WHEN("set is moved")
+        WHEN("iterate over set using iterator")
         {
-            Set movedSet = std::move(set);
-
-            THEN("the moved set is not empty")
+            THEN("all elements are accessible and sorted in ascending order")
             {
-                REQUIRE(movedSet.isEmpty() == false);
-                REQUIRE(movedSet.size() == kSetSize);
+                std::array<int, kElements.size()> sorted = kElements;
+                std::ranges::sort(sorted);
+
+                auto it = set.iterator();
+                size_t idx = 0;
+                while (it.hasNext())
+                {
+                    int value = it.next();
+                    REQUIRE(value == sorted[idx]);
+                    ++idx;
+                }
+                REQUIRE(idx == sorted.size());
+            }
+        }
+
+        WHEN("set is moved by constructor")
+        {
+            IntTreeSet destinationSet = std::move(set);
+
+            THEN("destination set is not empty")
+            {
+                REQUIRE(!destinationSet.isEmpty());
+                REQUIRE(destinationSet.size() == kElements.size());
             }
 
             THEN("original set is empty")
             {
-                REQUIRE(set.isEmpty() == true);
+                REQUIRE(set.isEmpty());
                 REQUIRE(set.size() == 0);
             }
 
             THEN("moved elements are accessible")
             {
-                for (int elem = kMinElem; elem <= kMaxElem; ++elem)
+                for (int elem : kElements)
                 {
-                    auto contains = movedSet.contains(elem);
-                    REQUIRE(contains == true);
+                    REQUIRE(destinationSet.contains(elem));
+                }
+            }
+        }
+
+        WHEN("set is moved by operator=")
+        {
+            IntTreeSet destinationSet;
+            destinationSet = std::move(set);
+
+            THEN("destination set is not empty")
+            {
+                REQUIRE(!destinationSet.isEmpty());
+                REQUIRE(destinationSet.size() == kElements.size());
+            }
+
+            THEN("original set is empty")
+            {
+                REQUIRE(set.isEmpty());
+                REQUIRE(set.size() == 0);
+            }
+
+            THEN("moved elements are accessible")
+            {
+                for (int elem : kElements)
+                {
+                    REQUIRE(destinationSet.contains(elem));
                 }
             }
         }
     }
 }
 
-SCENARIO("TreeSetIterator")
+namespace
 {
-    Set set;
-
-    GIVEN("empty set")
+    struct LifecycleCounter
     {
-        WHEN("TreeSetIterator is created")
+    public:
+        LifecycleCounter()
+            : m_id(++s_nextId)
         {
-            auto it = set.iterator();
+            ++s_constructCount;
+        }
 
-            THEN("hasNext returns false")
+        LifecycleCounter(LifecycleCounter&& other) noexcept
+            : m_id(other.m_id)
+        {
+            ++s_constructCount;
+        }
+
+        explicit LifecycleCounter(int id)
+            : m_id(id)
+        {
+            ++s_constructCount;
+        }
+
+        LifecycleCounter& operator=(LifecycleCounter&& other) noexcept = default;
+
+        ~LifecycleCounter()
+        {
+            ++s_destructCount;
+        }
+
+        LifecycleCounter(const LifecycleCounter&) = delete;
+        LifecycleCounter& operator=(const LifecycleCounter&) = delete;
+
+        static void resetCounters()
+        {
+            s_constructCount = 0;
+            s_destructCount = 0;
+        }
+
+        static bool areAllObjectsDestructed()
+        {
+            return s_constructCount == s_destructCount;
+        }
+
+        friend bool operator<(const LifecycleCounter& lhs, const LifecycleCounter& rhs)
+        {
+            return lhs.m_id < rhs.m_id;
+        }
+
+    private:
+        int m_id = 0;
+        static inline int s_constructCount = 0;
+        static inline int s_destructCount = 0;
+        static inline int s_nextId = 0;
+    };
+
+    using LifecycleCounterTreeSet = kf::TreeSet<LifecycleCounter, PagedPool>;
+}
+
+SCENARIO("TreeSet: lifecycle check")
+{
+    LifecycleCounter::resetCounters();
+
+    GIVEN("set with one item")
+    {
+        LifecycleCounterTreeSet set;
+        const int id = 123;
+
+        REQUIRE_NT_SUCCESS(set.add(LifecycleCounter{id}));
+        REQUIRE(!LifecycleCounter::areAllObjectsDestructed());
+
+        WHEN("item is removed by value")
+        {
+            auto removed = set.remove(LifecycleCounter{id});
+
+            THEN("all objects are destructed")
             {
-                REQUIRE(it.hasNext() == false);
+                REQUIRE(removed);
+                REQUIRE(LifecycleCounter::areAllObjectsDestructed());
             }
+        }
 
-            THEN("next returns a reference to nullptr")
+        WHEN("set is cleared")
+        {
+            set.clear();
+
+            THEN("all objects are destructed")
             {
-                auto& next = it.next();
-                REQUIRE(&next == nullptr);
+                REQUIRE(LifecycleCounter::areAllObjectsDestructed());
             }
         }
     }
 
-    GIVEN("set with elements")
+    GIVEN("set with multiple items")
     {
-        for (int elem = kMinElem; elem <= kMaxElem; ++elem)
         {
-            REQUIRE_NT_SUCCESS(set.add(elem));
+            LifecycleCounterTreeSet set;
+
+            REQUIRE_NT_SUCCESS(set.add(LifecycleCounter{}));
+            REQUIRE_NT_SUCCESS(set.add(LifecycleCounter{}));
+            REQUIRE_NT_SUCCESS(set.add(LifecycleCounter{}));
+
+            REQUIRE(!LifecycleCounter::areAllObjectsDestructed());
         }
 
-        WHEN("TreeSetIterator is created")
+        WHEN("set has gone out of scope")
         {
-            auto it = set.iterator();
-
-            int expectedValue = kMinElem;
-            int count = 0;
-
-            THEN("it should enumerate all elements in order")
+            THEN("all objects are destructed")
             {
-                while (it.hasNext())
-                {
-                    int value = it.next();
-                    REQUIRE(value == expectedValue);
-                    ++expectedValue;
-                    ++count;
-                }
-                REQUIRE(count == (kMaxElem - kMinElem + 1));
+                REQUIRE(LifecycleCounter::areAllObjectsDestructed());
+            }
+        }
+    }
+
+    GIVEN("set with multiple items moved to another set")
+    {
+        LifecycleCounterTreeSet destinationSet;
+
+        {
+            LifecycleCounterTreeSet set;
+
+            REQUIRE_NT_SUCCESS(set.add(LifecycleCounter{}));
+            REQUIRE_NT_SUCCESS(set.add(LifecycleCounter{}));
+            REQUIRE_NT_SUCCESS(set.add(LifecycleCounter{}));
+
+            destinationSet = std::move(set);
+
+            REQUIRE(!LifecycleCounter::areAllObjectsDestructed());
+        }
+
+        WHEN("original set has gone out of scope but destination set lives")
+        {
+            THEN("all objects are not destructed")
+            {
+                REQUIRE(!LifecycleCounter::areAllObjectsDestructed());
+            }
+        }
+    }
+
+    GIVEN("set with multiple items moved to another set (destination goes out of scope)")
+    {
+        LifecycleCounterTreeSet localSet;
+
+        REQUIRE_NT_SUCCESS(localSet.add(LifecycleCounter{}));
+        REQUIRE_NT_SUCCESS(localSet.add(LifecycleCounter{}));
+        REQUIRE_NT_SUCCESS(localSet.add(LifecycleCounter{}));
+
+        REQUIRE(!LifecycleCounter::areAllObjectsDestructed());
+
+        {
+            LifecycleCounterTreeSet destinationSet = std::move(localSet);
+        }
+
+        WHEN("destination set has gone out of scope but original set lives")
+        {
+            THEN("all objects are destructed")
+            {
+                REQUIRE(LifecycleCounter::areAllObjectsDestructed());
             }
         }
     }
