d22eff4d3f313be2537cf7808edc1c55d40b9d9d
Sergey Podobry <sergius@apriorit.com>
2026-01-16 15:27:47 +0200

    Refactor tests

--- test/ThreadTest.cpp
+++ test/ThreadTest.cpp
@@ -3,152 +3,214 @@
 
 namespace
 {
+    struct ThreadContext
+    {
+        bool started = false;
+    };
+
+    void threadProc(void* context)
+    {
+        static_cast<ThreadContext*>(context)->started = true;
+        PsTerminateSystemThread(STATUS_SUCCESS);
+    }
+
     struct TestObject
     {
+        ThreadContext m_threadContext;
+        
         NTSTATUS run()
         {
-            value = 123;
+            m_threadContext.started = true;
             return STATUS_SUCCESS;
         }
-
-        int value = 0;
     };
 }
 
-SCENARIO("kf::Thread")
+SCENARIO("kf::Thread basic lifecycle")
 {
-    GIVEN("Default-constructed thread")
+    GIVEN("A default constructed thread")
     {
         kf::Thread thread;
 
-        WHEN("Starting a thread with lambda")
+        WHEN("join() is called without start()")
         {
-            int value = 0;
-            NTSTATUS status = thread.start([](void* context) {
-                auto p = static_cast<int*>(context);
-                *p = 123;
-                }, &value);
+            thread.join();
 
-            THEN("Status is successful")
+            THEN("it should be a no-op")
+            {
+                REQUIRE(true);
+            }
+        }
+
+        WHEN("start() is used with threadProc")
+        {
+            ThreadContext context;
+
+            const NTSTATUS status = thread.start(&threadProc, &context);
+
+            THEN("start() should succeed")
+            {
+                REQUIRE_NT_SUCCESS(status);
+            }
+
+            THEN("join() waits for completion")
+            {
+                thread.join();
+                REQUIRE(context.started);
+            }
+        }
+
+        WHEN("start() is used with lambda")
+        {
+            ThreadContext context;
+
+            NTSTATUS status = thread.start([](void* context)
+                {
+                    static_cast<ThreadContext*>(context)->started = true;
+                    PsTerminateSystemThread(STATUS_SUCCESS);
+                }, 
+                &context);
+
+            THEN("start() should succeed")
             {
                 REQUIRE(NT_SUCCESS(status));
             }
 
             thread.join();
 
-            THEN("Thread changed the given value")
+            THEN("lambda should run")
             {
-                REQUIRE(value == 123);
+                REQUIRE(context.started);
             }
         }
 
-        WHEN("Thread is not started and joined")
+        WHEN("start() is used with object member routine")
         {
+            TestObject obj;
+
+            const NTSTATUS status = thread.start<&TestObject::run>(&obj);
+
+            THEN("start() should succeed")
+            {
+                REQUIRE_NT_SUCCESS(status);
+            }
+
             thread.join();
 
-            THEN("Does nothing and no crash occurs")
+            THEN("member routine should run")
+            {
+                REQUIRE(obj.m_threadContext.started);
+            }
+        }
+
+        WHEN("calling join twice after start")
+        {
+            REQUIRE_NT_SUCCESS(thread.start([](void*) { PsTerminateSystemThread(STATUS_SUCCESS); }, nullptr));
+
+            thread.join();
+            thread.join();
+
+            THEN("it's safe to call join() multiple times")
             {
             }
         }
 
-        WHEN("Calling join twice after start")
+        WHEN("thread object is destroyed while running")
         {
-            REQUIRE_NT_SUCCESS(thread.start([](void*) { }, nullptr));
+            ThreadContext context;
 
-            thread.join();
-            thread.join();
-
-            THEN("Does nothing and no crash occurs")
             {
+                kf::Thread local;
+                REQUIRE_NT_SUCCESS(local.start(&threadProc, &context));
+            }
+
+            THEN("destructor should join and thread should have run")
+            {
+                REQUIRE(context.started);
+            }
+        }
+
+        WHEN("starting a thread with a long working function")
+        {
+            ThreadContext context;
+
+            REQUIRE_NT_SUCCESS(thread.start([](void* context)
+                {
+                    LARGE_INTEGER oneMillisecond{ .QuadPart = -10'000LL }; // 1ms
+                    KeDelayExecutionThread(KernelMode, false, &oneMillisecond);
+
+                    static_cast<ThreadContext*>(context)->started = true;
+                    PsTerminateSystemThread(STATUS_SUCCESS);
+                }, 
+                &context));
+
+            thread.join();
+
+            THEN("join() waits for completion")
+            {
+                thread.join();
+                REQUIRE(context.started);
+            }
+        }
+    }
+}
+
+SCENARIO("kf::Thread move constructor and move assignment")
+{
+    GIVEN("A started thread")
+    {
+        ThreadContext context;
+        kf::Thread thread1;
+        REQUIRE_NT_SUCCESS(thread1.start(&threadProc, &context));
+
+        WHEN("Thread is move-constructed")
+        {
+            kf::Thread thread2(std::move(thread1));
+
+            THEN("moved-to object should be able to join")
+            {
+                thread2.join();
+                REQUIRE(context.started);
+            }
+
+            THEN("moved-from object join() should be safe")
+            {
+                thread1.join();
+                REQUIRE(context.started);
             }
         }
     }
 
-    GIVEN("Thread with a member routine")
+    GIVEN("Two threads where left-hand side already owns a running thread")
     {
-        kf::Thread thread;
-        TestObject obj;
+        ThreadContext context1;
+        ThreadContext context2;
 
-        WHEN("Starting a thread with member function")
+        kf::Thread thread1;
+        kf::Thread thread2;
+
+        REQUIRE_NT_SUCCESS(thread1.start(&threadProc, &context1));
+        REQUIRE_NT_SUCCESS(thread2.start(&threadProc, &context2));
+
+        WHEN("Move-assigning thread1 = std::move(thread2)")
         {
-            NTSTATUS status = thread.start<&TestObject::run>(&obj);
+            thread1 = std::move(thread2);
 
-            THEN("Status is successful")
+            THEN("thread1 should join the thread it now owns")
             {
-                REQUIRE(NT_SUCCESS(status));
+                thread1.join();
+                REQUIRE(context2.started);
             }
 
-            thread.join();
-
-            THEN("Thread called the function")
+            THEN("thread2 should be in a valid empty state")
             {
-                REQUIRE(obj.value == 123);
+                thread2.join();
+                REQUIRE(true);
             }
-        }
-    }
 
-    GIVEN("Thread with long working function")
-    {
-        kf::Thread thread;
-        int value = 0;
-
-        WHEN("Starting a thread with long working function and joined")
-        {
-            REQUIRE_NT_SUCCESS(thread.start([](void* context) {
-                auto p = static_cast<int*>(context);
-                LARGE_INTEGER interval;
-                interval.QuadPart = -10'000;
-                KeDelayExecutionThread(KernelMode, FALSE, &interval);
-                *p = 246;
-                }, &value));
-
-            thread.join();
-
-            THEN("Main thread execution continued after given thread finished its work")
+            THEN("the original thread from thread1 should have executed")
             {
-                REQUIRE(value == 246);
-            }
-        }
-    }
-
-    GIVEN("Thread that goes out of scope")
-    {
-        int value = 0;
-        {
-            kf::Thread thread;
-            REQUIRE_NT_SUCCESS(thread.start([](void* context) {
-                auto p = static_cast<int*>(context);
-                *p = 777;
-                }, &value));
-        }
-
-        THEN("Destructor joined and work was finished")
-        {
-            REQUIRE(value == 777);
-        }
-    }
-
-    GIVEN("Working thread")
-    {
-        kf::Thread thread;
-        int value = 0;
-
-        REQUIRE_NT_SUCCESS(thread.start([](void* context) {
-            auto p = static_cast<int*>(context);
-            LARGE_INTEGER interval;
-            interval.QuadPart = -10'000;
-            KeDelayExecutionThread(KernelMode, FALSE, &interval);
-            *p = 999;
-            }, &value));
-
-        WHEN("Thread is moved to another instance")
-        {
-            kf::Thread thread2 = std::move(thread);
-            thread2.join();
-
-            THEN("Function form thread is completed")
-            {
-                REQUIRE(value == 999);
+                REQUIRE(context1.started);
             }
         }
     }
